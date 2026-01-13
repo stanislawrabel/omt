253fb85242a44cc9115123326fb025a7fcb1e362
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2026-01-13 20:54:08 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,4 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
+APP_NAME="OTA Multi Tools"
+APP_VERSION="1.0.0"
+APP_AUTHOR="Stano36"
+echo "   OTA Multi Tools $VERSION"
 
 echo -e "${GREEN}+=====================================+${RESET}"
 echo -e "${GREEN}|==${RESET} ${GREEN}${APP_NAME}${RESET} ${YELLOW}v${APP_VERSION}${RESET} ${GREEN}==|${RESET}"
@@ -6,9 +10,12 @@ echo -e "${GREEN}+=====================================+${RESET}"
 
 clear
 
-BASE_DIR="$HOME/ota-multi-tools"
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 while true; do
+
+
+
   clear
   echo "======================================"
   echo "      OTA Multi Tools  v1.0"
@@ -20,14 +27,32 @@ while true; do
   echo "0) Exit"
   echo "--------------------------------------"
 
-  read -p "Select option: " opt
+    read -p "Select option: " choice
 
-  case "$opt" in
-    1) bash "$BASE_DIR/finder.sh" ;;
-    2) bash "$BASE_DIR/share.sh" ;;
-    3) bash "$BASE_DIR/downloader.sh" ;;
-    4) bash "$BASE_DIR/universal_downloader.sh" ;;
-    0) exit 0 ;;
-    *) echo "❌ Invalid option"; sleep 1 ;;
+  case "$choice" in
+    1)
+      bash "$BASE_DIR/1.sh"
+      read -p "Press ENTER..."
+      ;;
+    2)
+      bash "$BASE_DIR/2.sh"
+      read -p "Press ENTER..."
+      ;;
+    3)
+      bash "$BASE_DIR/3.sh"
+      read -p "Press ENTER..."
+      ;;
+    4)
+      bash "$BASE_DIR/4.sh"
+      read -p "Press ENTER..."
+      ;;
+    0)
+      exit 0
+      ;;
+    *)
+      echo "❌ Invalid option"
+      sleep 1
+      ;;
   esac
 done
+
