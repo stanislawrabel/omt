979c792cd1a7d6c6f379b82cd0ced9e802aad5b8
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2026-01-15 16:19:18 +0100

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,4 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/bash
+# 🎨 Farby pre výstup
+WHITE="\033[37m"
+PURPLE="\033[35m" 
+YELLOW="\033[33m"
+BLUE="\033[34m"
+RED="\033[31m"
+BLACK="\033[30m"
+WHITE="\033[37m"
+GREEN="\033[32m"
+YELLOW_BG="\033[43m"
+GREEN_BG="\033[42m"
+RED_BG="\033[41m"
+RESET="\033[0m"
+
 APP_NAME="OTA Multi Tools"
 APP_VERSION="1.0.0"
 APP_AUTHOR="Stano36"
@@ -16,15 +30,15 @@ while true; do
 
 
   clear
-  echo -e "${GREEN}+${RESET}=======================================${GREEN}+${RESET}"
-  echo -e "${GREEN}|${RESET}      OTA Multi Tools  v1.0"         |
+  echo -e "${GREEN}+${RESET}=====================================${GREEN}+${RESET}"
+  echo -e "${GREEN}|${RESET}      OTA Multi Tools  v1.0.0           ${GREEN}|${RESET}"
   echo -e "${GREEN}|${RESET} ${YELLOW_BG}${BLACK}  realme   ${RESET}  ${GREEN_BG}${BLACK}  oppo  ${RESET}  ${RED_BG}${WHITE}  Oneplus  ${RESET}  ${GREEN}|${RESET}"
-  echo -e "${GREEN}+${RESET}======================================${GREEN}+${RESET}"
-  echo -e" ${GREEN}|${RESET}1) OTA FindeR                    ${GREEN}|${RESET}"
-  echo -e" ${GREEN}|${RESET}2) Share OTA links               ${GREEN}|${RESET}"
-  echo -e" ${GREEN}|${RESET}3) DownloadeR                    ${GREEN}|${RESET}"
-  echo -e "${GREEN}|${RESET}4) Universal DownloadeR & Resolver${GREEN}|${RESET}"
-  echo -e "${GREEN}|${RESET}0) Exit                       ${GREEN}|${RESET}"
+  echo -e "${GREEN}+${RESET}=====================================${GREEN}+${RESET}"
+  echo -e "${GREEN}|${RESET}1) OTA FindeR                      ${GREEN}|${RESET}"
+  echo -e "${GREEN}|${RESET}2) Share OTA links                 ${GREEN}|${RESET}"
+  echo -e "${GREEN}|${RESET}3) DownloadeR                      ${GREEN}|${RESET}"
+  echo -e "${GREEN}|${RESET}4) Universal DownloadeR & Resolver  ${GREEN}|${RESET}"
+  echo -e "${GREEN}|${RESET}0) Exit                           ${GREEN}|${RESET}"
   echo -e "${GREEN}+${RESET}--------------------------------------${GREEN}+${RESET}"
 
     read -p "Select option: " choice
