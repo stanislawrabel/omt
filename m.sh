28e183e414dd59a682ab6d3a7cff8c9a1fd54ec5
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2026-01-17 12:51:27 +0100

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,4 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
+
 # 🎨 Farby pre výstup
 WHITE="\033[37m"
 PURPLE="\033[35m" 
@@ -13,30 +15,19 @@ GREEN_BG="\033[42m"
 RED_BG="\033[41m"
 RESET="\033[0m"
 
+tmux has-session -t ota 2>/dev/null || tmux new-session -d -s ota
+
+while true; do
+
 APP_NAME="OTA Multi Tools"
 APP_VERSION="1.0.0"
 APP_AUTHOR="Stano36"
+
+
 echo -e "   OTA Multi Tools $VERSION"
 echo -e "${GREEN}+=====================================+${RESET}"
 echo -e "${GREEN}|==${RESET} ${GREEN}${APP_NAME}${RESET} ${YELLOW}v${APP_VERSION}${RESET} ${GREEN}==|${RESET}"
 echo -e "${GREEN}+=====================================+${RESET}"
-
-
-
-BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-start_downloader() {
-  if tmux has-session -t downloader 2>/dev/null; then
-    echo "⚠️ DownloadeR already running – attaching..."
-    sleep 1
-    tmux attach -t downloader
-  else
-    echo "📥 Starting DownloadeR in new session..."
-    sleep 1
-    tmux new-session -s downloader "bash 3.sh; exec bash"
-  fi
-}
-
 
 
 
@@ -49,18 +40,17 @@ start_downloader() {
   echo -e "${GREEN}|${RESET}1) OTA FindeR                        ${GREEN}|${RESET}"
   echo -e "${GREEN}|${RESET}2) Share OTA links                   ${GREEN}|${RESET}"
   echo -e "${GREEN}|${RESET}3) DownloadeR                        ${GREEN}|${RESET}"
-  echo -e "${GREEN}|${RESET}4) Universal DownloadeR & Resolver   ${GREEN}|${RESET}"
   echo -e "${GREEN}|${RESET}0) Exit                              ${GREEN}|${RESET}"
   echo -e "${GREEN}+-------------------------------------+${RESET}"
 
-    read -p "Select option: " choice
+  
+  read -p "Select: " c
 
- case "$choice" in
-  1) bash 1.sh ;;
-  2) bash 2.sh ;;
-  3) start_downloader ;;
-  4) bash 4.sh ;;
-  0) exit 0 ;;
-esac
+  case "$c" in
+    1) tmux new-window -t ota -n Finder "bash 1.sh" ;;
+    2) tmux new-window -t ota -n Share  "bash 2.sh" ;;
+    3) tmux new-window -t ota -n Download "bash 3.sh" ;;
+    0) tmux kill-session -t ota; exit ;;
+  esac
 done
-read -p "Press ENTER to return to menu..."
+
