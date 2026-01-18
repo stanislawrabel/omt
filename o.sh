9190585b5ab070565cd75df9bcb1aeeca83344f9
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2026-01-18 10:56:16 +0100

    Update o.sh

--- o.sh
+++ o.sh
@@ -2,23 +2,7 @@
 
 SESSION="ota"
 
-tmux has-session -t $SESSION 2>/dev/null || tmux new-session -d -s $SESSION
+tmux has-session -t "$SESSION" 2>/dev/null || \
+tmux new-session -d -s "$SESSION" "bash menu.sh"
 
-while true; do
-  clear
-  echo "OTA Multi Tools"
-  echo
-  echo "1) OTA Finder"
-  echo "2) Share OTA links"
-  echo "3) Downloader"
-  echo "0) Exit"
-  echo
-  read -r -p "Select: " c
-
-  case "$c" in
-    1) tmux new-window -t $SESSION -n Finder "bash 1.sh" ;;
-    2) tmux new-window -t $SESSION -n Share "bash 2.sh" ;;
-    3) tmux new-window -t $SESSION -n Download "bash 3.sh" ;;
-    0) tmux kill-session -t $SESSION; exit ;;
-  esac
-done
+tmux attach -t "$SESSION"
