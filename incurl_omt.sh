9a7b48641e416109160cf1c930ea0221bd1fda33
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2026-01-19 05:58:22 +0100

    Update incurl_omt.sh

--- incurl_omt.sh
+++ incurl_omt.sh
@@ -37,7 +37,8 @@ for file in o.sh m.sh 1.sh 2.sh 3.sh 4.sh models.txt devices.txt; do
 done
 
 echo "✅ All files downloaded successfully"
-chmod +x o.sh m.sh 1.sh 2.sh 3.sh 4.sh chmod +x ~/o.sh
+chmod +x o.sh m.sh 1.sh 2.sh 3.sh 4.sh 
+chmod +x ~/o.sh
 ln -sf ~/o.sh $PREF
 chmod +x m.sh
 # mv m.sh ~/bin/m
